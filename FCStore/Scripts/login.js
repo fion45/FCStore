22a438eb9547f603b87950a5c8d057366296a973
fionhuo <86945494@qq.com>
2014-05-16 00:17:09 +0800

    1

---
+++ FCStore/Scripts/login.js
@@ -0,0 +1,29 @@
+﻿$(function () {
+    $("#loginBtn").click(function (ev) {
+        var obj = {};
+        obj.userID = $("#UIDTB").val();
+        obj.PSW = $("#PSWTB").val();
+        obj.checkCode = $("#checkCodeTB").val();
+        if (obj.checkCode.length == 4) {
+            $.myAjax({
+                loadEle: $("#loginDiv"),
+                url: "/User/Login",
+                data: JSON.stringify(obj),
+                dataType: "json",
+                type: "POST",
+                contentType: "application/json;charset=utf-8",
+                success: function (data, status, options) {
+                    if (data == null) {
+                        alert("密码或者用户名错误，请重新输入！")
+                    }
+                    else {
+                        alert("登陆成功！");
+                    }
+                }
+            });
+        }
+        else {
+            alert("验证码错误！");
+        }
+    });
+});
