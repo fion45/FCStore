0ed0a78c34f2fa693b8d37e5595016c442f54c96
fionhuo <86945494@qq.com>
2014-07-27 23:25:51 +0800

    11111

---
+++ FCStore/Models/RecentView.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Xml.Serialization;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace FCStore.Models
+{
+    public class RecentView
+    {
+        [Key, DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        public int RVID
+        {
+            get;
+            set;
+        }
+
+        [ForeignKey("User")]
+        public int UID
+        {
+            get;
+            set;
+        }
+
+        public User User
+        {
+            get;
+            set;
+        }
+
+        [ForeignKey("Product")]
+        public int PID
+        {
+            get;
+            set;
+        }
+
+        public Product Product
+        {
+            get;
+            set;
+        }
+
+        public string ViewDT
+        {
+            get;
+            set;
+        }
+    }
+}
