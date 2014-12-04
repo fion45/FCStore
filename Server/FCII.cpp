87022876b0134bbcc10095a32a6293a7db0c0511
fionhuo <86945494@qq.com>
2014-12-05 00:28:26 +0800

    1111111111

--- Server/FCII.cpp
+++ Server/FCII.cpp
@@ -2,11 +2,57 @@
 #include "FCII.h"
 
 
-FCII::FCII()
+FCII::FCII(UINT8* stream, FCIIExecuter* executer)
 {
+	//判断是不是websocket数据流
+	CString tmpStr(stream);
+	m_isweb = tmpStr.Left(3) == _T("GET");
+	m_executer = executer;
 }
 
 
 FCII::~FCII()
 {
+	
+}
+
+bool FCII::Analyse(UINT8* stream,int streamLen, FCIIContent* content)
+{
+	content = (FCIIContent*)stream;
+	content->Data = content->Arg + content->ArgLen;
+	//检查长度
+	if (streamLen != content->ArgLen + content->DataLen + sizeof(UINT16)* 6 + sizeof(UINT32))
+		return false;
+	//检查checkSum
+	UINT16 tmpCS = 0;
+	for (int i = 0; i < streamLen; i++)
+	{
+		tmpCS += *(stream + i);
+	}
+	if (content->CheckSum != tmpCS)
+		return false;
+	return true;
+}
+
+bool FCII::ReceiveStream(UINT8* stream, bool* continueTag)
+{
+	if (m_isweb)
+	{
+
+	}
+}
+
+int FCII::SendStream(UINT8* stream, bool* continueTag)
+{
+
+	if (m_isweb)
+	{
+
+	}
+}
+
+
+FCIIContent* FCIIExecuter::Dispatch(FCIIContent* content)
+{
+
 }
--- Server/FCII.h
+++ Server/FCII.h
@@ -4,19 +4,39 @@ class FCII
 public:
 	bool m_isweb;
 
+	FCIIContent* m_currentcontent;
 public:
-	FCII();
+	FCII(UINT8* stream, FCIIExecuter* executer);
 	~FCII();
+
+	static bool Analyse(UINT8* stream, int streamLen, FCIIContent* content);
+
+	bool ReceiveStream(UINT8* stream, bool* continueTag);
+
+	int SendStream(UINT8* stream, bool* continueTag);
+private:
+	UINT8* m_currentreceive;
+	FCIIExecuter* m_executer;
 };
 
 struct FCIIContent
 {
 	UINT16	Identify;
-	UINT16	Code;
+	UINT16	CheckSum;
 	UINT16	MainCMD;
 	UINT16	SubCMD;
+	UINT16	ErrCode;
 	UINT16	ArgLen;
 	UINT32	DataLen;
-	UINT8*	ArgContent;
-	UINT8*	DataContent;
+
+	UINT8*	Arg;
+	UINT8*	Data;
+};
+
+class FCIIExecuter
+{
+public:
+	FCIIContent* Dispatch(FCIIContent* content);
+
+	virtual bool Login(CString UserID, CString PSW, CString CheckCode) = 0;
 };
